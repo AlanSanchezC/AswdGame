9f6843b837a2c04bcb79c2c6c02a5e750defc290
Alan Francisco Sánchez Cazarez <acazarez58@gmail.com>
2017-09-27 21:02:17 -0500

    awsdGame

--- Vidiogeim/Form1.cs
+++ Vidiogeim/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,63 @@ namespace Vidiogeim
         public Form1()
         {
             InitializeComponent();
+            s1 = new SoundPlayer(@"C:/bing.wav");
+            s2 = new SoundPlayer(@"C:/bong.wav");
+            r = new Random();
+
+            g = pctCanvas.CreateGraphics();
+            main = new Objeto(0, 0);            
+            Cursor.Hide();
+            empezar();
+        }
+
+        static Random r;
+        SoundPlayer s1,s2;
+        Graphics g;
+        Objeto main;
+        int contador;
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            g.Clear(Color.PaleGoldenrod);
+            main.avanzar(g, e.KeyChar);
+
+            Point nav = pctCanvas.Location;
+            nav.Offset(main.getX(), main.getY());
+            Cursor.Position = PointToScreen(nav);
+        }
+
+        private void empezar()
+        {
+            contador = 10;
+            Point Punto_Inicio = pctCanvas.Location;
+            Punto_Inicio.Offset(0, 0);
+            Cursor.Position = PointToScreen(Punto_Inicio);
+            main.reiniciar(g);
+
+            if (r.Next(1, 3) == 1)
+                s1.Play();
+            else
+                s2.Play();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        { 
+            lblCont.Text = contador.ToString();
+            contador--;
+           
+            if (contador < 0)
+                contador = 10;
+        }
+
+        private void Colission(object sender, EventArgs e)
+        {
+            empezar();
         }
     }
 }
---
+++ Vidiogeim/Objeto.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vidiogeim
+{
+    class Objeto
+    {
+        protected int x;
+        protected int y;
+        protected int size;
+
+        public Objeto(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            size = 20;
+        }
+
+        private void irArriba()
+        {
+            if (this.y == 0)
+                y = 580;
+            else
+                y -= size;
+        }
+
+        private void irAbajo()
+        {
+            if (this.y == 580)
+                y = 0;
+            else
+                y += size;
+        }
+
+        private void irIzquierda()
+        {
+            if (this.x == 0)
+                x = 780;
+            else
+                x -= size;
+        }
+
+        private void irDerecha()
+        {
+            if (this.x == 780)
+                x = 0;
+            else
+                x += size;
+            
+        }
+
+        public void avanzar(Graphics g, char d)
+        {
+            switch (d)
+            {
+                case 'w':
+                    this.irArriba();
+                    break;
+                case 's':
+                    this.irAbajo();
+                    break;
+                case 'd':
+                    this.irDerecha();
+                    break;
+                case 'a':
+                    this.irIzquierda();
+                    break;
+            }
+
+            g.FillRectangle(new SolidBrush(Color.Firebrick), this.x, this.y, size, size);
+        }
+
+        public void reiniciar(Graphics g)
+        {
+            this.x = 0;
+            this.y = 0;
+            g.FillRectangle(new SolidBrush(Color.Firebrick), this.x, this.y, size, size);
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+
+        public int getY()
+        {
+            return y;
+        }
+    }
+}
