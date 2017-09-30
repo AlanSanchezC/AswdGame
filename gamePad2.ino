fd62d6650d607086169940864d1e86a265949c7e
Alan Francisco Sánchez Cazarez <acazarez58@gmail.com>
2017-09-30 01:32:51 -0500

    Comentarios

--- gamePad2.ino
+++ gamePad2.ino
@@ -1,13 +1,19 @@
+//Hecho por: Alan Sánchez Cazarez
+
+//Se necesita la librería Keyboard.h (Solo Arduino Leonardo, Micro o Due)
 #include <Keyboard.h>
 
-int btnIzquierda = 3;
-int btnArriba = 4;
-int btnAbajo = 5;
-int btnDerecha = 6;
+int btnIzquierda = 3; //Pin digital 3
+int btnArriba = 4; //Pin digital 4
+int btnAbajo = 5; //Pin digital 5
+int btnDerecha = 6; //Pin digital 6
 
 int state = 1;
 
 void setup() {
+  /*
+  Modo INPUT_PULLUP para detectar los pulsos BAJOS
+  */
   pinMode(btnIzquierda, INPUT_PULLUP);
   pinMode(btnArriba, INPUT_PULLUP);
   pinMode(btnAbajo, INPUT_PULLUP);
@@ -16,13 +22,16 @@ void setup() {
 }
 
 void loop(){
+  //Se detecta el estado del pin (0 ó 1)
   state = digitalRead(btnIzquierda);
   
+  //Siempre estará en 1 (ALTO), pero al presionar el botón, dará un pulso 0 (BAJO)
   if (state != 1){
     Keyboard.write('a');
     delay(250);
   }
 
+  //Mismo código para todas las teclas
   state = digitalRead(btnArriba);
   
   if (state != 1){
@@ -44,5 +53,3 @@ void loop(){
     delay(250);
   }
 }
-
-
