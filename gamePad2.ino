69c7cd5f817e8ba320d11507766ea46b9b68dad1
Alan Francisco Sánchez Cazarez <acazarez58@gmail.com>
2017-09-30 01:26:53 -0500

    Control USB 4 botones Arduino Leonardo

---
+++ gamePad2.ino
@@ -0,0 +1,48 @@
+#include <Keyboard.h>
+
+int btnIzquierda = 3;
+int btnArriba = 4;
+int btnAbajo = 5;
+int btnDerecha = 6;
+
+int state = 1;
+
+void setup() {
+  pinMode(btnIzquierda, INPUT_PULLUP);
+  pinMode(btnArriba, INPUT_PULLUP);
+  pinMode(btnAbajo, INPUT_PULLUP);
+  pinMode(btnDerecha, INPUT_PULLUP);
+  Keyboard.begin();
+}
+
+void loop(){
+  state = digitalRead(btnIzquierda);
+  
+  if (state != 1){
+    Keyboard.write('a');
+    delay(250);
+  }
+
+  state = digitalRead(btnArriba);
+  
+  if (state != 1){
+    Keyboard.write('w');
+    delay(250);
+  }
+
+  state = digitalRead(btnAbajo);
+  
+  if (state != 1){
+    Keyboard.write('s');
+    delay(250);
+  }
+
+  state = digitalRead(btnDerecha);
+  
+  if (state != 1){
+    Keyboard.write('d');
+    delay(250);
+  }
+}
+
+
